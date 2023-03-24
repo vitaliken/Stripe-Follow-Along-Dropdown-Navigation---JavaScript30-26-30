d97ef173c6b2f8dbfec646c806cb5fbe8fda6e57
Vitaly Hutarau <vitaliken@gmail.com>
2023-03-24 23:13:45 +0300

    add event listeners

--- scripts.js
+++ scripts.js
@@ -1,3 +1,14 @@
 const triggers = document.querySelectorAll('.cool > li')
 const background = document.querySelector('.dropdownBackground')
 const nav = document.querySelector('.top')
+
+function handleEnter() {
+    console.log('enter')
+}
+
+function handleLeave() {
+    console.log('leave')
+}
+
+triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
+triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave));
